fa90983754cc9771198372aa61ddc966fff12bcc
robwayne <robwaynegordon@gmail.com>
2017-12-04 12:24:43 +0400

    Initial Commit

---
+++ InstagramClone/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  InstagramClone
+//
+//  Created by Robert Gordon on 12/4/17.
+//  Copyright © 2017 Robert Gordon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
